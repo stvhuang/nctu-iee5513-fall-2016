a032071197c34b32ad7939825dd1ea0b857f6a9d
Yanjia Huang <yanjia.huang.tw@gmail.com>
2016-11-29 01:58:34 +0800

    Update
    CudaMalloc and cudaMemcpy of convolution are implemented, and also
    revise some coding style.

--- final_project_p1/cnnConvLayer.cu
+++ final_project_p1/cnnConvLayer.cu
@@ -1,113 +1,155 @@
 // This program executes a typical convolutional layer in regular CNNs
 #include <iostream>
 #include "cnnConvLayer.h"
+
 using namespace std;
 
 // This is the CPU version, please don't modify it
 void convLayerCPU()
 {
-	// declarations for bunch of indexing parameters
-	int fn, sli, fmy, fmx, y, x;
-	int sum, ifmy, ifmx, ofmy, ofmx;
-	int filtIdx, inNeuIdx, outNeuIdx, outIdx;
-	int filtVol = FMDEPTH * FILTSIZE * FILTSIZE;
-	int filtArea = FILTSIZE * FILTSIZE;
-	int fmArea = FMSIZE *FMSIZE;
-	int outArea = FMSIZE/2 * FMSIZE/2;
+    // declarations for bunch of indexing parameters
+    int fn, sli, fmy, fmx, y, x;
+    int sum, ifmy, ifmx, ofmy, ofmx;
+    int filtIdx, inNeuIdx, outNeuIdx, outIdx;
+    int filtVol = FMDEPTH * FILTSIZE * FILTSIZE;
+    int filtArea = FILTSIZE * FILTSIZE;
+    int fmArea = FMSIZE *FMSIZE;
+    int outArea = FMSIZE/2 * FMSIZE/2;
 
-	// Convolution
-	for(fn = 0; fn < FILTNUM; fn++){
-		for(fmy = 0; fmy < FMSIZE; fmy += STRIDE){
-			for(fmx = 0; fmx < FMSIZE; fmx += STRIDE){
-				sum = 0;
-				for(sli = 0; sli < FMDEPTH; sli++){
-					for(y = 0; y < FILTSIZE; y++){
-						for(x = 0; x < FILTSIZE; x++){
-							ifmy = fmy - FILTSIZE / 2 + y;
-							ifmx = fmx - FILTSIZE / 2 + x;
-							filtIdx = fn*filtVol + sli*filtArea + y*FILTSIZE + x;
-							inNeuIdx = sli*fmArea + ifmy*FMSIZE + ifmx;
-							if(ifmy > 0 && ifmy < FMSIZE && ifmx > 0 && ifmx < FMSIZE)
-								sum += filt[filtIdx] * inNeu[inNeuIdx];
-						}
-					}
-				}
-				// Activation - ReLU
-				outNeuIdx = fn*fmArea + fmy*FMSIZE + fmx;
-				if(sum <= 0)
-					outNeu[outNeuIdx] = 0;
-				else
-					outNeu[outNeuIdx] = sum;
-			}
-		}
-	}
+    // Convolution1
+    for (fn = 0; fn < FILTNUM; fn++)
+    {
+        for (fmy = 0; fmy < FMSIZE; fmy += STRIDE)
+        {
+            for (fmx = 0; fmx < FMSIZE; fmx += STRIDE)
+            {
+                sum = 0;
+                for (sli = 0; sli < FMDEPTH; sli++)
+                {
+                    for (y = 0; y < FILTSIZE; y++)
+                    {
+                        for (x = 0; x < FILTSIZE; x++)
+                        {
+                            ifmy = fmy - FILTSIZE / 2 + y;
+                            ifmx = fmx - FILTSIZE / 2 + x;
+                            filtIdx = fn*filtVol + sli*filtArea + y*FILTSIZE + x;
+                            inNeuIdx = sli*fmArea + ifmy*FMSIZE + ifmx;
+                            if (ifmy > 0 && ifmy < FMSIZE && ifmx > 0 && ifmx < FMSIZE)
+                            {
+                                sum += filt[filtIdx] * inNeu[inNeuIdx];
+                            }
+                        }
+                    }
+                }
+                // Activation - ReLU
+                outNeuIdx = fn*fmArea + fmy*FMSIZE + fmx;
+                if (sum <= 0)
+                {
+                    outNeu[outNeuIdx] = 0;
+                }
+                else
+                {
+                    outNeu[outNeuIdx] = sum;
+                }
+            }
+        }
+    }
 
-	// Max Pooling with Window Size 2x2
-	int max, tmpVal;
-	for(sli = 0; sli < FILTNUM; sli++){
-		for(fmy = 0; fmy < FMSIZE/2 ; fmy += 1){
-			for(fmx = 0; fmx < FMSIZE/2 ; fmx += 1){
-				outNeuIdx = sli*fmArea + fmy*2*FMSIZE + fmx*2;
-				max = outNeu[outNeuIdx];
-				for(y = 0; y < 2; y++){
-					for(x = 0; x < 2; x++){
-						ofmy = fmy*2 + y;
-						ofmx = fmx*2 + x;
-						outNeuIdx = sli*fmArea + ofmy*FMSIZE + ofmx;
-						tmpVal = outNeu[outNeuIdx];	
-						if(tmpVal > max)
-							max = tmpVal;
-					}
-				}
-				outIdx = sli*outArea + fmy*FMSIZE/2 + fmx;
-				outCPU[outIdx] = max;
-			}
-		}
-	}
+    // Max Pooling with Window Size 2x2
+    int max, tmpVal;
+    for (sli = 0; sli < FILTNUM; sli++)
+    {
+        for (fmy = 0; fmy < FMSIZE/2 ; fmy += 1)
+        {
+            for (fmx = 0; fmx < FMSIZE/2 ; fmx += 1)
+            {
+                outNeuIdx = sli*fmArea + fmy*2*FMSIZE + fmx*2;
+                max = outNeu[outNeuIdx];
+                for (y = 0; y < 2; y++)
+                {
+                    for (x = 0; x < 2; x++)
+                    {
+                        ofmy = fmy*2 + y;
+                        ofmx = fmx*2 + x;
+                        outNeuIdx = sli*fmArea + ofmy*FMSIZE + ofmx;
+                        tmpVal = outNeu[outNeuIdx];
+                        if (tmpVal > max)
+                        {
+                            max = tmpVal;
+                        }
+                    }
+                }
+                outIdx = sli*outArea + fmy*FMSIZE/2 + fmx;
+                outCPU[outIdx] = max;
+            }
+        }
+    }
 }
 
-/***	Implement your CUDA Kernel here	***/
-__global__
-void convLayerGPU()
+void initGPU() // Allocate memory on device and copy data
 {
+    int inputNeuronSize = sizeof(short) * FMSIZE * FMSIZE * FMDEPTH;
+    int filtersSize = sizeof(short) * FILTNUM * FILTSIZE * FILTSIZE * FMDEPTH;
+    int outputNeuronSize = sizeof(int) * FMSIZE * FMSIZE * FMDEPTH;
+    int outputSize = sizeof(int) * FMSIZE / 2 * FMSIZE / 2 * FMDEPTH;
+
+    cudaMalloc(&devInputNeuron, inputNeuronSize);
+    cudaMalloc(&devInputFilter, filtersSize);
+    cudaMalloc(&devOutputNeuron, outputNeuronSize);
+    cudaMalloc(&devOutput, outputSize);
+
+    cudaMemcpy(devInputNeuron, inNeu, inputNeuronSize, cudaMemcpyHostToDevice);
+    cudaMemcpy(devInputFilter, filt, filtersSize, cudaMemcpyHostToDevice);
 }
-/***	Implement your CUDA Kernel here	***/
+
+/*** Implement your CUDA Kernel here ***/
+__global__
+void convLayerGPU(short * inNeuron, short * filters, int * outNuron, int * out)
+{
+    // convolution
+}
+/*** Implement your CUDA Kernel here ***/
 
 int main()
 {
-	int convLayerCPUExecTime, convLayerGPUExecTime;
-	init();
-		
-	timespec time_begin, time_end;                                                 
-  clock_gettime(CLOCK_REALTIME, &time_begin);
+    int convLayerCPUExecTime, convLayerGPUExecTime;
+    init(); // Initialize the data on host memory
 
-	convLayerCPU();
-
-  clock_gettime(CLOCK_REALTIME, &time_end);
-	convLayerCPUExecTime = timespec_diff_us(time_begin, time_end);
-	cout << "CPU time for executing a typical convolutional layer = " 
-			 <<  convLayerCPUExecTime / 1000 << "ms" << endl;
+    timespec time_begin, time_end;
 
   clock_gettime(CLOCK_REALTIME, &time_begin);
-	/***	Lunch your CUDA Kernel here	***/
-
-	convLayerGPU<<<1,1>>>(); // Lunch the kernel
-	
-	cudaDeviceSynchronize(); // Do synchronization before clock_gettime()
-	/***	Lunch your CUDA Kernel here	***/
+    convLayerCPU();
   clock_gettime(CLOCK_REALTIME, &time_end);
-	convLayerGPUExecTime = timespec_diff_us(time_begin, time_end);
-	cout << "GPU time for executing a typical convolutional layer = " 
-			 << convLayerGPUExecTime / 1000 << "ms" << endl;
+    convLayerCPUExecTime = timespec_diff_us(time_begin, time_end);
+    cout << "CPU time for executing a typical convolutional layer = " <<  convLayerCPUExecTime / 1000 << "ms" << endl;
 
-	if(checker()){
-		cout << "Congratulations! You pass the check." << endl;
-		cout << "Speedup: " << (float)convLayerCPUExecTime / convLayerGPUExecTime << endl;
-	}
-	else
-		cout << "Sorry! Your result is wrong." << endl;
+    initGPU();
+  clock_gettime(CLOCK_REALTIME, &time_begin);
+    /***    Lunch your CUDA Kernel here    ***/
+    convLayerGPU<<<1, 1>>>(devInputNeuron, devInputFilter, devOutputNeuron, devOutput); // Lunch the kernel
+    cudaDeviceSynchronize(); // Do synchronization before clock_gettime()
+    /***    Lunch your CUDA Kernel here    ***/
+  clock_gettime(CLOCK_REALTIME, &time_end);
+    convLayerGPUExecTime = timespec_diff_us(time_begin, time_end);
 
-	ending();
-	
-	return 0;
+    cout << "GPU time for executing a typical convolutional layer = " << convLayerGPUExecTime / 1000 << "ms" << endl;
+
+    if (checker())
+    {
+        cout << "Congratulations! You pass the check." << endl;
+        cout << "Speedup: " << (float)convLayerCPUExecTime / convLayerGPUExecTime << endl;
+    }
+    else
+    {
+        cout << "Sorry! Your result is wrong." << endl;
+    }
+
+    cudaFree(&devInputNeuron);
+    cudaFree(&devOutputNeuron);
+    cudaFree(&devOutput);
+    cudaFree(&devInputFilter);
+
+    ending();
+
+    return 0;
 }
