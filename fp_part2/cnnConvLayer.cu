cab8a77b523a5b7e23e459f9df7c5cbd25a1e8be
Yanjia Huang <yanjia.huang.tw@gmail.com>
2017-01-16 02:41:57 +0800

    Update

--- fp_part2/cnnConvLayer.cu
+++ fp_part2/cnnConvLayer.cu
@@ -1,4 +1,5 @@
 // This program executes a typical convolutional layer in regular CNNs
+// 0230
 #include <iostream>
 #include "cnnConvLayer.h"
 using namespace std;
@@ -12,85 +13,108 @@ void convLayerCPU()
     int filtIdx, inNeuIdx, outNeuIdx, outIdx;
     int filtVol = FMDEPTH * FILTSIZE * FILTSIZE;
     int filtArea = FILTSIZE * FILTSIZE;
-    int fmArea = FMSIZE * FMSIZE;
-    int outArea = FMSIZE / 2 * FMSIZE / 2;
+    int fmArea = FMSIZE *FMSIZE;
+    int outArea = FMSIZE/2 * FMSIZE/2;
 
     // Convolution
-    for (fn = 0; fn < FILTNUM; ++fn)
-    {
-        for (fmy = 0; fmy < FMSIZE; fmy += STRIDE)
-        {
-            for (fmx = 0; fmx < FMSIZE; fmx += STRIDE)
-            {
+    for(fn = 0; fn < FILTNUM; fn++){
+        for(fmy = 0; fmy < FMSIZE; fmy += STRIDE){
+            for(fmx = 0; fmx < FMSIZE; fmx += STRIDE){
                 sum = 0;
-                for (sli = 0; sli < FMDEPTH; ++sli)
-                {
-                    for (y = 0; y < FILTSIZE; ++y)
-                    {
-                        for (x = 0; x < FILTSIZE; ++x)
-                        {
+                for(sli = 0; sli < FMDEPTH; sli++){
+                    for(y = 0; y < FILTSIZE; y++){
+                        for(x = 0; x < FILTSIZE; x++){
                             ifmy = fmy - FILTSIZE / 2 + y;
                             ifmx = fmx - FILTSIZE / 2 + x;
-                            filtIdx = fn * filtVol + sli * filtArea + y * FILTSIZE + x;
-                            inNeuIdx = sli * fmArea + ifmy * FMSIZE + ifmx;
-                            if (ifmy >= 0 && ifmy < FMSIZE && ifmx >= 0 && ifmx < FMSIZE)
-                            {
+                            filtIdx = fn*filtVol + sli*filtArea + y*FILTSIZE + x;
+                            inNeuIdx = sli*fmArea + ifmy*FMSIZE + ifmx;
+                            if(ifmy >= 0 && ifmy < FMSIZE && ifmx >= 0 && ifmx < FMSIZE)
                                 sum += filt[filtIdx] * inNeu[inNeuIdx];
-                            }
                         }
                     }
                 }
                 // Activation - ReLU
-                outNeuIdx = fn * fmArea + fmy * FMSIZE + fmx;
-                if (sum <= 0)
-                {
+                outNeuIdx = fn*fmArea + fmy*FMSIZE + fmx;
+                if(sum <= 0)
                     outNeu[outNeuIdx] = 0;
-                }
                 else
-                {
                     outNeu[outNeuIdx] = sum;
-                }
             }
         }
     }
 
     // Max Pooling with Window Size 2x2
     int max, tmpVal;
-    for ( sli = 0; sli < FILTNUM; ++sli)
-    {
-        for (fmy = 0; fmy < FMSIZE/2 ; ++fmy)
-        {
-            for (fmx = 0; fmx < FMSIZE/2 ; ++fmx)
-            {
-                outNeuIdx = sli * fmArea + fmy * 2 * FMSIZE + fmx * 2;
+    for(sli = 0; sli < FILTNUM; sli++){
+        for(fmy = 0; fmy < FMSIZE/2 ; fmy += 1){
+            for(fmx = 0; fmx < FMSIZE/2 ; fmx += 1){
+                outNeuIdx = sli*fmArea + fmy*2*FMSIZE + fmx*2;
                 max = outNeu[outNeuIdx];
-                for (y = 0; y < 2; ++y)
-                {
-                    for (x = 0; x < 2; ++x)
-                    {
-                        ofmy = fmy * 2 + y;
-                        ofmx = fmx * 2 + x;
-                        outNeuIdx = sli * fmArea + ofmy * FMSIZE + ofmx;
+                for(y = 0; y < 2; y++){
+                    for(x = 0; x < 2; x++){
+                        ofmy = fmy*2 + y;
+                        ofmx = fmx*2 + x;
+                        outNeuIdx = sli*fmArea + ofmy*FMSIZE + ofmx;
                         tmpVal = outNeu[outNeuIdx];
-                        if (tmpVal > max)
-                        {
+                        if(tmpVal > max)
                             max = tmpVal;
-                        }
                     }
                 }
-                outIdx = sli * outArea + fmy * FMSIZE / 2 + fmx;
+                outIdx = sli*outArea + fmy*FMSIZE/2 + fmx;
                 outCPU[outIdx] = max;
             }
         }
     }
 }
 
-/***   Implement your CUDA Kernel here   ***/
+/***    Implement your CUDA Kernel here    ***/
 __global__
-void convLayerGPU()
+void convLayerGPU(const short *filtCooData, const short *filtCooRow, const short *filtCooCol, const short *inNeuCooData, const short *inNeuCooRow, const short *inNeuCooCol, int *devOut)
 {
+    __shared__ int sum[34][34];
+
+    int inY(0), inX(0), offY(0), offX(0), max(0);
+    const int BLOCKID(blockIdx.x), THREADID(threadIdx.x), \
+              outY(THREADID / 16), outX(THREADID % 16), \
+              outIdx(BLOCKID * 16 * 16 + outY * 16 + outX);
+
+    sum[THREADID % 32 + 1][THREADID / 32 + 1] = 0; // clear the middle(32*32) data in sum
+    __syncthreads();
+
+    for (int i(0); i < 512; ++i)
+    {
+        if (THREADID < 204)
+        {
+            offY = 1 - filtCooRow[BLOCKID * 512 + i];
+            offX = 1 - filtCooCol[BLOCKID * 512 + i];
+            inY = inNeuCooRow[i * 204 + THREADID] + offY; // inY ranges from -1 to 32
+            inX = inNeuCooCol[i * 204 + THREADID] + offX; // inX ranges from -1 to 32
+            sum[inY + 1][inX + 1] += filtCooData[BLOCKID * 512 + i] * inNeuCooData[i * 204 + THREADID];
+        }
+        __syncthreads();
+    }
+
+    __syncthreads();
+
+    // Max Pooling
+    if (THREADID < 256)
+    {
+        for (int i(0); i < 2; ++i)
+        {
+            for (int j(0); j < 2; ++j)
+            {
+                inY = outY * 2 + i + 1; // ranges from 0 to 31
+                inX = outX * 2 + j + 1; // ranges from 0 to 31
+                if (sum[inY][inX] > max)
+                {
+                    max = sum[inY][inX];
+                }
+            }
+        }
+        devOut[outIdx] = max;
+    }
 }
-/***   Implement your CUDA Kernel here   ***/
+/***    Implement your CUDA Kernel here    ***/
 
 int main()
 {
@@ -105,28 +129,48 @@ int main()
 
   clock_gettime(CLOCK_REALTIME, &time_end);
     convLayerCPUExecTime = timespec_diff_us(time_begin, time_end);
-    cout << "CPU time for executing a typical convolutional layer = " <<  convLayerCPUExecTime / 1000 << "ms" << endl;
+    cout << "CPU time for executing a typical convolutional layer = "
+             <<  convLayerCPUExecTime / 1000 << "ms" << endl;
+
+///////////////////////////////
+    cudaMalloc(&devFiltCooData, sizeof(short) * 512 * 512); // 512 filters, with only one nnz data per slice(512 in total)
+    cudaMalloc(&devFiltCooRow, sizeof(short) * 512 * 512);
+    cudaMalloc(&devFiltCooCol, sizeof(short) * 512 * 512);
+    cudaMalloc(&devInNeuCooData, sizeof(short) * 204 * 512);
+    cudaMalloc(&devInNeuCooRow, sizeof(short) * 204 * 512);
+    cudaMalloc(&devInNeuCooCol, sizeof(short) * 204 * 512);
+    cudaMalloc(&devOut, sizeof(int) * 16 * 116 * 512);
+///////////////////////////////
 
   clock_gettime(CLOCK_REALTIME, &time_begin);
-    /***   Lunch your CUDA Kernel here   ***/
 
-    convLayerGPU<<<1, 1>>>(); // Lunch the kernel
+///////////////////////////////
+    cudaMemcpy(devFiltCooData, filtCooData, sizeof(short) * 512 * 512, cudaMemcpyHostToDevice);
+    cudaMemcpy(devFiltCooRow, filtCooRow, sizeof(short) * 512 * 512, cudaMemcpyHostToDevice);
+    cudaMemcpy(devFiltCooCol, filtCooCol, sizeof(short) * 512 * 512, cudaMemcpyHostToDevice);
+    cudaMemcpy(devInNeuCooData, inNeuCooData, sizeof(short) * 204 * 512, cudaMemcpyHostToDevice);
+    cudaMemcpy(devInNeuCooRow, inNeuCooRow, sizeof(short) * 204 * 512, cudaMemcpyHostToDevice);
+    cudaMemcpy(devInNeuCooCol, inNeuCooCol, sizeof(short) * 204 * 512, cudaMemcpyHostToDevice);
+///////////////////////////////
 
+    /***    Lunch your CUDA Kernel here    ***/
+    convLayerGPU<<<512, 1024>>>(devFiltCooData, devFiltCooRow, devFiltCooCol, devInNeuCooData, devInNeuCooRow, devInNeuCooCol, devOut); // Lunch the kernel
     cudaDeviceSynchronize(); // Do synchronization before clock_gettime()
-    /***   Lunch your CUDA Kernel here   ***/
+    /***    Lunch your CUDA Kernel here    ***/
   clock_gettime(CLOCK_REALTIME, &time_end);
-    convLayerGPUExecTime = timespec_diff_us(time_begin, time_end);
-    cout << "GPU time for executing a typical convolutional layer = " << convLayerGPUExecTime / 1000 << "ms" << endl;
 
-    if (checker())
-    {
+    cudaMemcpy(outGPU, devOut, sizeof(int) * 16 * 16 * 512, cudaMemcpyDeviceToHost);
+
+    convLayerGPUExecTime = timespec_diff_us(time_begin, time_end);
+    cout << "GPU time for executing a typical convolutional layer = "
+             << convLayerGPUExecTime / 1000 << "ms" << endl;
+
+    if(checker()){
         cout << "Congratulations! You pass the check." << endl;
         cout << "Speedup: " << (float)convLayerCPUExecTime / convLayerGPUExecTime << endl;
     }
     else
-    {
         cout << "Sorry! Your result is wrong." << endl;
-    }
 
     ending();
 
--- fp_part2/cnnConvLayer.h
+++ fp_part2/cnnConvLayer.h
@@ -1,11 +1,10 @@
 #ifndef __CNNCONVLAYER_H__
 #define __CNNCONVLAYER_H__
-
+// 0230
 #include <cstdlib>
 #include <iostream>
 #include <string>
 #include <fstream>
-
 using namespace std;
 
 #define FMSIZE 32
@@ -14,21 +13,34 @@ using namespace std;
 #define FILTNUM 512
 #define STRIDE 1
 
-short * filt;
-short * inNeu;
-int * outNeu;
-int * outCPU;
-int * outGPU;
+short *filt;
+short *inNeu;
+int *outNeu;
+int *outCPU;
+int *outGPU;
 
-short * filtCooNNZ;
-short * filtCooData;
-short * filtCooRow;
-short * filtCooCol;
+short *filtCooNNZ;
+short *filtCooData;
+short *filtCooRow;
+short *filtCooCol;
 
-short * inNeuCooNNZ;
-short * inNeuCooData;
-short * inNeuCooRow;
-short * inNeuCooCol;
+short *inNeuCooNNZ;
+short *inNeuCooData;
+short *inNeuCooRow;
+short *inNeuCooCol;
+
+///////////////////////////////
+short *inNeuG;
+short *devFiltCooData;
+short *devFiltCooRow;
+short *devFiltCooCol;
+
+short *devInNeuCooData;
+short *devInNeuCooRow;
+short *devInNeuCooCol;
+
+int *devOut;
+///////////////////////////////
 
 void init()
 {
@@ -38,27 +50,22 @@ void init()
     int filtTensorVol = FILTNUM * FMDEPTH * FILTSIZE * FILTSIZE;
     int inNeuVol = FMDEPTH * FMSIZE * FMSIZE;
     int outNeuVol = FILTNUM * FMSIZE * FMSIZE;
-    int outVol = FILTNUM * FMSIZE / 2 * FMSIZE / 2;
+    int outVol = FILTNUM * FMSIZE/2 * FMSIZE/2;
 
     fstream ifs;
 
     inNeu = new short [inNeuVol]();
     ifs.open("data/inNeu.txt", ifstream::in);
-    if (!ifs.is_open())
-    {
+    if(!ifs.is_open()){
         cout << "Can not open the neurons input file\n";
         exit(-1);
     }
-
-    for (i = 0; i < FMDEPTH; ++i)
-    {
+    for(i = 0; i < FMDEPTH; i++){
         ifs >> str;
-        for (j = 0; j < FMSIZE; ++j)
-        {
-            for (k = 0; k < FMSIZE; ++k)
-            {
+        for(j = 0; j < FMSIZE; j++){
+            for(k = 0; k < FMSIZE; k++){
                 ifs >> tmp;
-                inNeuIdx = i * FMSIZE * FMSIZE + j * FMSIZE + k;
+                inNeuIdx = i*FMSIZE*FMSIZE + j*FMSIZE + k;
                 inNeu[inNeuIdx] = tmp;
             }
         }
@@ -67,24 +74,19 @@ void init()
 
     filt = new short [filtTensorVol]();
     ifs.open("data/filt.txt", ifstream::in);
-    if (!ifs.is_open())
-    {
+    if(!ifs.is_open()){
         cout << "Can not open the filters input file\n";
         exit(-1);
     }
-
-    for (i = 0; i < FILTNUM; ++i)
-    {
+    for(i = 0; i < FILTNUM; i++){
         ifs >> str;
-        for (j = 0; j < FMDEPTH; ++j)
-        {
+        for(j = 0; j < FMDEPTH; j++){
             ifs >> str;
-            for (k = 0; k < FILTSIZE; ++k)
-            {
-                for (l = 0; l < FILTSIZE; ++l)
-                {
+            for(k = 0; k < FILTSIZE; k++){
+                for(l = 0; l < FILTSIZE; l++){
                     ifs >> tmp;
-                    filtIdx = i * FMDEPTH * FILTSIZE * FILTSIZE + j * FILTSIZE * FILTSIZE + k * FILTSIZE + l;
+                    filtIdx = i*FMDEPTH*FILTSIZE*FILTSIZE + j*FILTSIZE*FILTSIZE
+                                        + k*FILTSIZE + l;
                     filt[filtIdx] = tmp;
                 }
             }
@@ -93,6 +95,7 @@ void init()
     ifs.close();
 
     outNeu = new int [outNeuVol]();
+
     outCPU = new int [outVol]();
     outGPU = new int [outVol]();
 }
@@ -105,49 +108,38 @@ void initCoo()
 
     fstream ifs;
 
-    filtCooNNZ = new short [FILTNUM * FMDEPTH];
+    filtCooNNZ = new short [FILTNUM*FMDEPTH];
 
     ifs.open("data/filt.coo", ifstream::in);
-    if (!ifs.is_open())
-    {
+    if(!ifs.is_open()){
         cout << "Can not open the filters input file\n";
         exit(-1);
     }
-
-    for (i = 0; i < FILTNUM; ++i)
-    {
+    for(i = 0; i < FILTNUM; i++){
         ifs >> str;
-        for (j = 0; j < FMDEPTH; ++j)
-        {
+        for(j = 0; j < FMDEPTH; j++){
             ifs >> str;
             ifs >> str >> nnz;
-            idx = i * FMDEPTH + j;
+            idx = i*FMDEPTH + j;
             filtCooNNZ[idx] = nnz;
-            if (i == 0 && j==0)
-            {
-                filtCooData = new short [FILTNUM * FMDEPTH * nnz];
-                filtCooRow = new short [FILTNUM * FMDEPTH * nnz];
-                filtCooCol = new short [FILTNUM * FMDEPTH * nnz];
+            if(i == 0 && j==0){
+                filtCooData = new short [FILTNUM*FMDEPTH*nnz];
+                filtCooRow = new short [FILTNUM*FMDEPTH*nnz];
+                filtCooCol = new short [FILTNUM*FMDEPTH*nnz];
             }
-
-            for (k = 0; k < nnz; ++k)
-            {
+            for(k = 0; k < nnz; k++){
                 ifs >> str >> tmp;
-                idx = i * FMDEPTH * nnz + j * nnz + k;
+                idx = i*FMDEPTH*nnz + j*nnz + k;
                 filtCooData[idx] = tmp;
             }
-
-            for (k = 0; k < nnz; ++k)
-            {
+            for(k = 0; k < nnz; k++){
                 ifs >> str >> tmp;
-                idx = i * FMDEPTH * nnz + j * nnz + k;
+                idx = i*FMDEPTH*nnz + j*nnz + k;
                 filtCooRow[idx] = tmp;
             }
-
-            for (k = 0; k < nnz; ++k)
-            {
+            for(k = 0; k < nnz; k++){
                 ifs >> str >> tmp;
-                idx = i * FMDEPTH * nnz + j * nnz + k;
+                idx = i*FMDEPTH*nnz + j*nnz + k;
                 filtCooCol[idx] = tmp;
             }
         }
@@ -157,45 +149,36 @@ void initCoo()
     inNeuCooNNZ = new short [FMDEPTH];
 
     ifs.open("data/inNeu.coo", ifstream::in);
-    if (!ifs.is_open())
-    {
+    if(!ifs.is_open()){
         cout << "Can not open the neurons input file\n";
         exit(-1);
     }
-
-    for (i = 0; i < FMDEPTH; ++i)
-    {
+    for(i = 0; i < FMDEPTH; i++){
         ifs >> str;
         ifs >> str >> nnz;
         inNeuCooNNZ[i] = nnz;
-        if (i == 0)
-        {
-            inNeuCooData = new short [FMDEPTH * nnz];
-            inNeuCooRow = new short [FMDEPTH * nnz];
-            inNeuCooCol = new short [FMDEPTH * nnz];
+        if(i == 0){
+            inNeuCooData = new short [FMDEPTH*nnz];
+            inNeuCooRow = new short [FMDEPTH*nnz];
+            inNeuCooCol = new short [FMDEPTH*nnz];
         }
 
         ifs >> str;
-        for (j = 0; j < nnz; ++j)
-        {
+        for(j = 0; j < nnz; j++){
             ifs >> tmp;
-            idx = i * nnz + j;
+            idx = i*nnz + j;
             inNeuCooData[idx] = tmp;
         }
-
         ifs >> str;
-        for (j = 0; j < nnz; ++j)
-        {
+        for(j = 0; j < nnz; j++){
             ifs >> tmp;
-            idx = i * nnz + j;
+            idx = i*nnz + j;
             inNeuCooRow[idx] = tmp;
         }
-
         ifs >> str;
-        for (j = 0; j < nnz; ++j)
-        {
+        for(j = 0; j < nnz; j++){
             ifs >> tmp;
-            idx = i * nnz + j;
+            idx = i*nnz + j;
             inNeuCooCol[idx] = tmp;
         }
     }
@@ -219,29 +202,41 @@ void ending()
     delete [] inNeuCooData;
     delete [] inNeuCooRow;
     delete [] inNeuCooCol;
+
+///////////////////////////////
+    cudaFree(&devFiltCooData);
+    cudaFree(&devFiltCooRow);
+    cudaFree(&devFiltCooCol);
+
+    cudaFree(&devInNeuCooData);
+    cudaFree(&devInNeuCooRow);
+    cudaFree(&devInNeuCooCol);
+
+    cudaFree(&inNeuG); // what is this
+
+    cudaFree(&devOut); // done
+///////////////////////////////
 }
 
-bool checker()
-{
+bool checker(){
     int i;
-    int outVol = FILTNUM * FMSIZE / 2 * FMSIZE / 2;
+    int outVol = FILTNUM * FMSIZE/2 * FMSIZE/2;
 
-    for (i = 0; i < outVol; ++i)
-    {
-        if (outCPU[i] != outGPU[i])
-        {
+    for(i = 0; i < outVol; i++){
+        if(outCPU[i] != outGPU[i]){
             cout << "The element: " << i << " is wrong!\n";
             cout << "outCPU[" << i << "] = " << outCPU[i] << endl;
             cout << "outGPU[" << i << "] = " << outGPU[i] << endl;
             return false;
         }
     }
+
     return true;
 }
 
-int timespec_diff_us(timespec & t1, timespec & t2)
+int timespec_diff_us(timespec& t1, timespec& t2)
 {
-    return (t2.tv_sec - t1.tv_sec) * 1e6 + (t2.tv_nsec - t1.tv_nsec) / 1e3;
+  return (t2.tv_sec - t1.tv_sec) * 1e6 + (t2.tv_nsec - t1.tv_nsec) / 1e3;
 }
 
 #endif
